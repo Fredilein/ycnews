2c27e9ca97c979898a9eddcd791ff4000b02a050
Adrian Zanga <adrian.zanga@icloud.com>
2019-05-08 18:32:28 +0200

    support webapp

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -8,10 +8,10 @@
     <span class="meta">
       <div v-if="url">
         <div v-if="ncomments > 1">
-          {{ time | timeAgo }} ago | <i>{{ url | host }}</i> | <a v-on:click="goToComments(id)" href="#">{{ ncomments }} <i class="far fa-comments fa-sm"></i></a>
+          {{ time | timeAgo }} ago | <i>{{ url | host }}</i> | <a v-on:click="goToComments(id)">{{ ncomments }} <i class="far fa-comments fa-sm"></i></a>
         </div>
         <div v-else-if="ncomments > 0">
-          {{ time | timeAgo }} ago | <i>{{ url | host }}</i> | <a v-on:click="goToComments(id)" href="#">{{ ncomments }} <i class="far fa-comment fa-sm"></i></a>
+          {{ time | timeAgo }} ago | <i>{{ url | host }}</i> | <a v-on:click="goToComments(id)">{{ ncomments }} <i class="far fa-comment fa-sm"></i></a>
         </div>
         <div v-else>
           {{ time | timeAgo }} ago | <i>{{ url | host }}</i>
